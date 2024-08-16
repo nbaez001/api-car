d9e2ff5d1847cfd364451d3a010302040bcc0333
nbaez001 <nbaez001@gmail.com>
2024-08-16 18:23:33 -0500

    Fixing SonarQube Issue for Maintainability

--- src/main/java/com/empresa/proyecto/car/model/Car.java
+++ src/main/java/com/empresa/proyecto/car/model/Car.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.relational.core.mapping.Column;
 
 @Getter
 @Setter
@@ -17,12 +18,13 @@ public class Car implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@Column(value = "ID")
 	private Long id;
-
+	@Column(value = "MODEL")
 	private String model;
-
+	@Column(value = "BRAND")
 	private String brand;
-
+	@Column(value = "COLOR")
 	private String color;
 
 }
